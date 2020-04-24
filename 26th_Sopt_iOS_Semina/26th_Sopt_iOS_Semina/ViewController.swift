143104d8f1aca6c0a3a872aae5f390ae914421f5
iJoom <ghfdld@naver.com>
2020-04-25 01:52:59 +0900

    practice 2th semina

---
+++ 26th_Sopt_iOS_Semina/26th_Sopt_iOS_Semina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  26th_Sopt_iOS_Semina
+//
+//  Created by IJ . on 2020/04/25.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
